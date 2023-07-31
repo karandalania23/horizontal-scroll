bc8a06c9552b07330e6a962b885ac5027e38be3f
Karan Dalania <karandalania.kd@gmail.com>
2023-07-31 22:28:40 +0530

    Add scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(100%)' },
+        }
+      },
+      animation : {
+        'slide-infinite' : 'slide 10s linear infinite',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
